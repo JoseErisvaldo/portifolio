8903c93a0a9163d15fde99f3102a03f8bb30f500
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-04-27 20:50:56 -0300

    Feat: onClink Btn

--- src/Components/UIComponents/Button/button.jsx
+++ src/Components/UIComponents/Button/button.jsx
@@ -1,6 +1,9 @@
-export default function Button({ button, color, colorText }) {
+export default function Button({ button, color, colorText, onClick }) {
   return (
-    <button className={`mt-6 bg-${color}-600 p-3 rounded text-${colorText}`}>
+    <button
+      onClick={onClick}
+      className={`mt-6 bg-${color}-600 p-3 rounded text-${colorText}`}
+    >
       {button}
     </button>
   )
