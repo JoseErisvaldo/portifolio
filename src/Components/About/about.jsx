a42aa91941d59e294d5be67505eed6fb39beed74
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-04-27 20:27:45 -0300

    Feat: about

---
+++ src/Components/About/about.jsx
@@ -0,0 +1,63 @@
+import H2 from '../UIComponents/h2/h2'
+
+export default function About() {
+  return (
+    <div className="p-3">
+      <div>
+        <h2 className="text-2xl font-bold mb-3">Sobre</h2>
+        <h2 className="text-1xl  font-bold mb-3">
+          Olá, eu sou o José, um desenvolvedor Front-End, apaixonado pelo que
+          faz!
+        </h2>
+        <ul>
+          <li>
+            👨🏻‍💻 Eu tive meu primeiro contato com linhas de código aos 22 anos de
+            idade, quando queria criar um robo para investimentos na bolsa de
+            valores. Depois de muita pesquisa consegui desenvolver. Desde então,
+            eu decidi que eu queria me aprofundar na linguagem de progamação —
+            eu queria ser um programador.
+          </li>
+          <li>
+            🚀 Atualmente, tenho como foco principal da minha carreira as áreas
+            de desenvolvimento web. Utilizo a stack JavaScript (React) para
+            desenvolver em ambas as frentes.
+          </li>
+          <li>
+            🕹️ Sou apaixonado por games. É o que eu mais gosto de fazer pra me
+            divertir e esvaziar a cabeça haha!
+          </li>
+        </ul>
+        <div className="mt-3">
+          <strong className="">Front-end:</strong>
+          <ul className="mt-3 flex gap-3 flex-wrap">
+            <li>
+              <img src="https://camo.githubusercontent.com/6f6bb7d3854c737e7548f212bdd17ccbea61c6ed644bd8da58c07c0d7d3c7602/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d48544d4c2d3333333333333f7374796c653d666c6174266c6f676f3d48544d4c35" />
+            </li>
+            <li>
+              <img src="https://camo.githubusercontent.com/1ee3a2eb805895d501c6e4e294f539d91491b65e2dfaf8a240a87f501b0d88fa/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4353532d3333333333333f7374796c653d666c6174266c6f676f3d43535333266c6f676f436f6c6f723d313537324236" />
+            </li>
+            <li>
+              <img src="https://camo.githubusercontent.com/7533cf04f3aee7c09a42eec7cc35795091924e730e82893068eb0c1c58d2c5ce/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4a6176615363726970742d3333333333333f7374796c653d666c6174266c6f676f3d6a617661736372697074" />
+            </li>
+            <li>
+              <img src="https://camo.githubusercontent.com/b0749033346cef8f69c697728cf56f94f8cfdf88cbae0cdbfb6f1bdb9cd918f3/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d547970655363726970742d3333333333333f7374796c653d666c6174266c6f676f3d74797065736372697074266c6f676f436f6c6f723d324437394337" />
+            </li>
+            <li>
+              <img src="https://camo.githubusercontent.com/e959d558b208794d7c904321f11894e636a01e925084c9d0b67ef671418e46e3/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d52656163742d3333333333333f7374796c653d666c6174266c6f676f3d7265616374" />
+            </li>
+            <li>
+              <img src="https://camo.githubusercontent.com/4dace74c265e57f407601e09b2530a09c50bd2054f7e2d20bc08f877887b8658/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4a6573742d3333333333333f7374796c653d666c6174266c6f676f3d6a657374266c6f676f436f6c6f723d453533354142" />
+            </li>
+          </ul>
+        </div>
+        <div className="mt-3">
+          <strong className="">🚀 Um pouco mais sobre o meu perfil</strong>
+          <div>
+            <img src="https://camo.githubusercontent.com/7e63ddba986a82ef4a69a9a5e20d264b9f3e79b24133ab4a3a0c55ce95760a23/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170693f757365726e616d653d6a6f73656572697376616c646f2673686f775f69636f6e733d74727565267468656d653d64726163756c61" />
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  )
+}
