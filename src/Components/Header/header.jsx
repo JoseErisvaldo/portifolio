a3769bbcb6c0eb7e895b137b85d122980bdd526f
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-04-27 20:27:20 -0300

    Feat: navegação das pages

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,13 +1,39 @@
+// Header.js
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Header() {
+export default function Header({ onNavChange }) {
+  const [nav, setNav] = useState('sobre')
+
+  const handleNavChange = newNav => {
+    setNav(newNav)
+    onNavChange(newNav)
+  }
+
   return (
     <div className="flex justify-center lg:justify-end mt-4 lg:mt-0 p-4">
-      <Link className="mx-1 bg-blue-400 p-2 rounded hover:bg-blue-600 text-white">
+      <Link
+        onClick={() => handleNavChange('sobre')}
+        className={`mx-1 bg-blue-400 p-2 rounded ${
+          nav === 'sobre' ? 'bg-blue-600' : 'hover:bg-blue-600'
+        } text-white`}
+      >
         Sobre
       </Link>
-      <Link className="mx-1 bg-blue-600 p-2 rounded text-white">Projetos</Link>
-      <Link className="mx-1 bg-blue-400 p-2 rounded hover:bg-blue-600 text-white">
+      <Link
+        onClick={() => handleNavChange('projetos')}
+        className={`mx-1 bg-blue-400 p-2 rounded ${
+          nav === 'projetos' ? 'bg-blue-600' : 'hover:bg-blue-600'
+        } text-white`}
+      >
+        Projetos
+      </Link>
+      <Link
+        onClick={() => handleNavChange('contato')}
+        className={`mx-1 bg-blue-400 p-2 rounded ${
+          nav === 'contato' ? 'bg-blue-600' : 'hover:bg-blue-600'
+        } text-white`}
+      >
         Contato
       </Link>
     </div>
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,20 +1,33 @@
+// Home.js
+import { useState } from 'react'
+import About from '../../Components/About/about'
 import Header from '../../Components/Header/header'
 import Projetos from '../../Components/Projetos/projetos'
 import SideBar from '../../Components/SideBar/sideBar'
 import H2 from '../../Components/UIComponents/h2/h2'
+import Contact from '../../Components/Contact/contact'
 
 export default function Home() {
+  const [nav, setNav] = useState('sobre')
+
+  const handleNavChange = newNav => {
+    setNav(newNav)
+  }
+
   return (
     <div className="bg-[#f3f4f6] min-h-screen p-8">
       <div className="bg-white max-w-5xl mx-auto rounded-lg shadow">
         <div className="flex flex-col lg:flex-row">
           <SideBar />
           <div className="lg:w-2/3">
-            <Header />
+            <Header onNavChange={handleNavChange} />
             <div className="px-8">
               <H2 h2={'Portifolio'} />
             </div>
-            <Projetos />
+
+            {nav === 'sobre' && <About />}
+            {nav === 'projetos' && <Projetos />}
+            {nav === 'contato' && <Contact />}
           </div>
         </div>
       </div>
